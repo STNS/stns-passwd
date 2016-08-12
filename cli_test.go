d51d30c53ff6873f3f42aa21847fd11f9312840e
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-08-13 05:41:52 +0900

    Remove old (unnecessary) tests

--- cli_test.go
+++ cli_test.go
@@ -22,30 +22,3 @@ func TestRun_versionFlag(t *testing.T) {
 		t.Errorf("expected %q to eq %q", errStream.String(), expected)
 	}
 }
-
-func TestRun_sFlag(t *testing.T) {
-	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
-	cli := &CLI{outStream: outStream, errStream: errStream}
-	args := strings.Split("./stns-passwd -s", " ")
-
-	status := cli.Run(args)
-	_ = status
-}
-
-func TestRun_cFlag(t *testing.T) {
-	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
-	cli := &CLI{outStream: outStream, errStream: errStream}
-	args := strings.Split("./stns-passwd -c", " ")
-
-	status := cli.Run(args)
-	_ = status
-}
-
-func TestRun_mFlag(t *testing.T) {
-	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
-	cli := &CLI{outStream: outStream, errStream: errStream}
-	args := strings.Split("./stns-passwd -m", " ")
-
-	status := cli.Run(args)
-	_ = status
-}
