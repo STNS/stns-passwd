f19c85308d735c9a85bea3d9def4e5fa824e5bff
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-08-13 06:52:41 +0900

    Add test for empty password

--- cli_test.go
+++ cli_test.go
@@ -41,3 +41,23 @@ func TestRun(t *testing.T) {
 		t.Error("outStream must not be empty")
 	}
 }
+
+func TestRun_emptyPassword(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./stns-passwd ", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeError {
+		t.Errorf("expected %d to eq %d", status, ExitCodeError)
+	}
+
+	expected := "Please specify a password"
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("expected %q to eq %q", errStream.String(), expected)
+	}
+
+	if len(outStream.String()) != 0 {
+		t.Error("outStream must be empty")
+	}
+}
